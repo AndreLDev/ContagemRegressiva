ec6d042eae901ea61b8877668fd3f313fbd7cd88
Tarde Cetafaju <Tarde.Cetafaju@labfies.org.br>
2023-12-28 16:52:28 -0300

    last commits

--- src/App.jsx
+++ src/App.jsx
@@ -3,20 +3,21 @@ import {Title} from './components/Title/Title'
 import {Contador} from './components/Contador/Contador'
 import newYear from './assets/newYear.jpg'
 import './App.css'
+import userCountDown from './hooks/userCountDown'
 
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [day, hour, minute, second] = userCountDown('Jan 1, 2024 00:00')
 
   return (
-     <div className='App' style={{backgroundImage:(`url(${newYear})`)}}>
+     <div className='App' style={{backgroundImage:(`url('https://s1.1zoom.me/b5050/257/Fireworks_523615_1920x1080.jpg')`)}}>
       <div className='container'>
         <Title title={'Contagem Regressiva'} />
         <div className='contagem-regressiva'>
-         <Contador title={'Dias'} number={2}></Contador>
-         <Contador title={'Horas'} number={2}></Contador>
-         <Contador title={'Minutos'} number={2}></Contador>
-         <Contador title={'Segundos'} number={2}></Contador>
+         <Contador title={'Dias'} number={day}></Contador>
+         <Contador title={'Horas'} number={hour}></Contador>
+         <Contador title={'Minutos'} number={minute}></Contador>
+         <Contador title={'Segundos'} number={second}></Contador>
         </div>
       </div>
         
--- src/components/Title/Title.jsx
+++ src/components/Title/Title.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import '../Title/Style.css'
 
 
 export const Title = ({title}) => {
